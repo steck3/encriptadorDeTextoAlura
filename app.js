5ffc2eea66af8c3a6145adf0d89040d468dcd3e7
Enrique Ochoa <150300119@ucaribe.edu.mx>
2024-08-27 18:00:48 -0500

    logica encriptar

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-let palabra ;
+const llaves = [
+    ["e","enter"],
+    ["i","imes"],
+    ["a","ai"],
+    ["o","ober"],
+    ["u","ufat"]
+];
+function encriptaMensaje(){
+    let mensajeEncriptado ="";
+    for(let i=0; i< mensajeEncriptado.length; i++){
+        let letra = mensaje.length[i];
+        let palablraEncriptada = letra;
+        for (let j =0; j < llaves.length; j++){
+            if (letra === llaves[j][0]){
+                palablraEncriptada =llaves[j][1];
+            break;
+            }   
+        }
+        mensajeEncriptado +=palablraEncriptada;
+    }
+    return mensajeEncriptado;
+}
+
+let letra;
